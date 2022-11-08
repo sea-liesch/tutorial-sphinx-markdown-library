aa3c75d906f8cbd48fde1ed23debc66d3045877b
Chuck Liesch <chuck_liesch@wycliffeassociates.org>
2022-11-08 21:51:53 +0000

    RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,11 +13,13 @@ release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import sphinx_rtd_theme
 
 extensions = [
     "myst_parser",
     "sphinx.ext.duration",
     "sphinx.ext.autosectionlabel",
+    'sphinx_rtd_theme',
 
 ]
 
@@ -29,5 +31,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
